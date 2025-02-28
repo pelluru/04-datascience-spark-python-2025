8055c734d04042137f1994a2d7d731ca45abed51
prabhakara pelluru <prabhakara.pelluru@gmail.com>
2025-02-27 16:19:14 -0800

    added

--- 01-prabha-pyspark-interviews/src/python/validate_ip.py
+++ 01-prabha-pyspark-interviews/src/python/validate_ip.py
@@ -1,9 +1,13 @@
-ipaddress = "1.23.53.1"
+import ipaddress
+
+
+ipaddress_ip = "1.23.53.1"
 
 
 def check_ip_validation(ipaddress):
     # Correct syntax for splitting an IP address
-    four_digits = ipaddress.split(",")
+    four_digits = ipaddress.split('.')
+
 
     # Ensure there are exactly four parts
     if len(four_digits) != 4:
@@ -20,6 +24,28 @@ def check_ip_validation(ipaddress):
     return True  # If all checks pass, return True
 
 
-valid_ip: bool = check_ip_validation(ipaddress)
+valid_ip: bool = check_ip_validation(ipaddress_ip)
 
 print(valid_ip)
+
+
+def check_ip_validation2(ipaddress_str):
+    """
+    Checks if a given string is a valid IPv4 address.
+
+    Args:
+        ipaddress_str: The string to check.
+
+    Returns:
+        True if the string is a valid IPv4 address, False otherwise.
+    """
+    try:
+        ipaddress.IPv4Address(ipaddress_str)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+valid_ip2: bool = check_ip_validation2(ipaddress)
+
+print(valid_ip2)
